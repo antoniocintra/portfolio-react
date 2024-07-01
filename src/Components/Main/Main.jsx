ea2640f4ac1b51475fb9cc3ee734910642833edf
Antônio <aecf12@hotmail.com>
2024-07-01 17:10:19 -0300

    add   function on buttons and curriculo.pdf

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,6 +1,16 @@
 import './Main.css';
 
 const Main = () => {
+
+    const handleCurriculoClick = () => {
+        window.open('/curriculo.pdf', '_blank');
+    };
+
+    const handleContatoClick = () => {
+        
+        window.open('https://wa.me/5511997166116', '_blank');
+    };
+
     return (
         <main>
             <div className='welcome-container'>
@@ -8,8 +18,8 @@ const Main = () => {
                     <h1><span>Prazer, me chamo Antônio!</span></h1>
                     <p>Desenvolvedor FullStack.</p>
                     <div className='action-container'>
-                        <button className='btn-curriculo'> Currículo</button>
-                            <button className='btn-contato'>Contato</button>
+                        <button className='btn-curriculo'  onClick={handleCurriculoClick}> Currículo</button>
+                            <button className='btn-contato' onClick={handleContatoClick}>Contato</button>
                         
                     </div>
                 </div>
